9a44edc6231df865aa7294111344b03c0c69e568
jlew92 <77688482+jlew92@users.noreply.github.com>
2021-02-28 21:13:06 +0300

    GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,15 @@
 package com.company;
 
+import GGUI.loginGUI;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws CloneNotSupportedException {
 	// write your code here
-        DemoSetup demoSetup = new DemoSetup();
-
+        //DemoSetup demoSetup = new DemoSetup();
+        new loginGUI();
 
 
 
