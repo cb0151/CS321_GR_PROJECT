e39c3a65bbfef1ff50eda7d2345fe24bade9f927
cb0151 <43941020+cb0151@users.noreply.github.com>
2021-03-04 08:30:16 -0600

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,5 @@ public class Main {
 
         DemoSetup demoSetup = new DemoSetup();
        new loginGUI();
-
-
-
-
-
-<<<<<<< HEAD
-=======
-       // DemoSetup demoSetup = new DemoSetup();
-        //new loginGUI();
-
->>>>>>> f57abd2d2d7b67c042ac48944327f93b34bcffa9
-
     }
 }
